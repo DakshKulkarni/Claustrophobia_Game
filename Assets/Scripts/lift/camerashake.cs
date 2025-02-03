bc4c5ea90555e1bd148dadfb74856e2a2c0058da
abhee238 <abhiram.vattikonda@gmail.com>
2025-02-03 21:09:14 +0530

    extreme shake

--- Assets/Scripts/lift/camerashake.cs
+++ Assets/Scripts/lift/camerashake.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class camerashake : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform camTransform;
+
+    // How long the object should shake for.
+    public float shakeDuration = 1f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.3f;
+    public float decreaseFactor = 2.0f;
+
+    private float timer;
+
+    Vector3 originalPos;
+
+    void Awake()
     {
-        
+        if (camTransform == null)
+        {
+            camTransform = GetComponent(typeof(Transform)) as Transform;
+        }
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        originalPos = camTransform.localPosition;
+    }
+
+
+
     void Update()
     {
-        
+        if (Mathf.FloorToInt(timer % 60) > 5)
+        {
+            if (shakeDuration > 0)
+            {
+                camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+                shakeDuration -= Time.deltaTime * decreaseFactor;
+            }
+            else
+            {
+                shakeDuration = 0f;
+                camTransform.localPosition = originalPos;
+                timer = 0;
+            }
+        }
+
+        timer += Time.deltaTime;    
     }
 }
